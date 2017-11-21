572e9899e7f67a6bf7c9be17c834def73baed72e
linzaixian <linzaixian888@qq.com>
2017-11-21 17:44:06 +0800

    例子精简

--- elasticsearch/src/main/java/com/linzaixian/demo/elasticsearch/Main.java
+++ elasticsearch/src/main/java/com/linzaixian/demo/elasticsearch/Main.java
@@ -6,6 +6,9 @@ import java.net.UnknownHostException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequestBuilder;
@@ -15,6 +18,7 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
@@ -23,13 +27,6 @@ import org.elasticsearch.transport.client.PreBuiltTransportClient;
 public class Main {
 	private  static SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     public static void main(String[] args) throws Exception {
-
-//
-//        TransportClient client = getTransportClient();
-//        XContentBuilder builder = XContentFactory.jsonBuilder().startObject().field("user", "kimchy").field("postDate", new Date())
-//                .field("message", "trying out Elasticsearch").endObject();
-//        IndexResponse response = client.prepareIndex("index", "type", "2").setSource(builder).get();
-//        System.out.println(response.status());
     	insert();
     	insert2();
     	queryEqual();
@@ -85,6 +82,8 @@ public class Main {
 								      .actionGet();  
     	System.out.println(response);
     }
+    
+    
     private static void insert2 () throws Exception {
     	TransportClient client=getTransportClient();
     	XContentBuilder builder=XContentFactory.jsonBuilder().startObject()  
@@ -98,56 +97,29 @@ public class Main {
 								      .actionGet();  
     	System.out.println(response);
     }
-//    private static void query() throws UnknownHostException {
-//    	
-//    	QueryBuilder qb=QueryBuilders.termsQuery("name.keyword", "abcab c中");
-//    	QueryBuilder qb2=QueryBuilders.boolQuery().must(qb);
-//    	TransportClient client=getTransportClient();;
-//    	SearchRequestBuilder builder=client.prepareSearch("index").setTypes("type");
-//    	SearchResponse response=builder.setQuery(qb2).execute().actionGet();
-//    	System.out.println(builder.toString());
-//    	System.out.println(response);
-//    	for(SearchHit hit:response.getHits()) {
-//    		System.out.println(hit.getSourceAsString());
-//    	}
-//    }
-//    
-//    private static void query(String index,String type,Map<String,Object> param) throws UnknownHostException {
-//    	BoolQueryBuilder queryBuilder=QueryBuilders.boolQuery();
-//		if(param!=null) {
-//			Iterator<Entry<String, Object>> it=param.entrySet().iterator();
-//			while(it.hasNext()) {
-//				Entry<String, Object> entry=it.next();
-//				queryBuilder.must(QueryBuilders.termQuery(entry.getKey()+".keyword", entry.getValue()));
-//			}
-//		}
-//    	TransportClient client=getTransportClient();
-//		SearchRequestBuilder searchRequestBuilder=client.prepareSearch(index).setTypes(type);
-//		System.out.println(queryBuilder);
-//		SearchResponse response=searchRequestBuilder.setQuery(queryBuilder).execute().actionGet();
-//		System.out.println(searchRequestBuilder);
-//		System.out.println(response);
-//    }
-//    private static void batchInsert() throws UnknownHostException {
-//    	Long count = 100000L;
-//    	String index = "bigdata";
-//    	String type = "student1";
-//    	TransportClient client=getTransportClient();
-//    	BulkRequestBuilder bulkRequest = client.prepareBulk();
-//    	for (int i = 0; i < count; i++) {
-//    	    Map<String, Object> ret = new HashMap<String, Object>();
-//    	    ret.put("recordtime", 11);
-//    	    ret.put("area", 22);
-//    	    ret.put("usertype", 33);
-//    	    ret.put("count", 44);
-//    	    bulkRequest.add(client.prepareIndex(index, type).setSource(ret));
-//    	    // 每10000条提交一次
-//    	    if (i % 10000 == 0) {
-//    	    	System.out.println("开始插入");
-//    	        bulkRequest.execute().actionGet();
-//    	    }
-//    	}
-//    }
+    /**
+     * 多条件精确查询
+     * @param index
+     * @param type
+     * @param param
+     * @throws UnknownHostException
+     */
+    private static void query(String index,String type,Map<String,Object> param) throws UnknownHostException {
+    	BoolQueryBuilder queryBuilder=QueryBuilders.boolQuery();
+		if(param!=null) {
+			Iterator<Entry<String, Object>> it=param.entrySet().iterator();
+			while(it.hasNext()) {
+				Entry<String, Object> entry=it.next();
+				queryBuilder.must(QueryBuilders.termQuery(entry.getKey()+".keyword", entry.getValue()));
+			}
+		}
+    	TransportClient client=getTransportClient();
+		SearchRequestBuilder searchRequestBuilder=client.prepareSearch(index).setTypes(type);
+		System.out.println(queryBuilder);
+		SearchResponse response=searchRequestBuilder.setQuery(queryBuilder).execute().actionGet();
+		System.out.println(searchRequestBuilder);
+		System.out.println(response);
+    }
     private static TransportClient getTransportClient() throws UnknownHostException {
     	 Settings setting = Settings.builder()
                  .put("cluster.name", "elasticsearch")//指定集群名称  
