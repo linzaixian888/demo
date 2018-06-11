93c258498f15811299ec0b78d8891a10cbf39c1c
linzaixian <linzaixian888@qq.com>
2018-06-11 11:33:43 +0800

    增加demo例子

--- spring-redis/src/main/java/com/linzaixian/demo/redis/SpringRedisTest.java
+++ spring-redis/src/main/java/com/linzaixian/demo/redis/SpringRedisTest.java
@@ -17,15 +17,21 @@ package com.linzaixian.demo.redis;
  *          </pre>
  */
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStringCommands.SetOption;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.types.Expiration;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -34,10 +40,30 @@ public class SpringRedisTest {
 	private static final  Logger logger=LoggerFactory.getLogger(SpringRedisTest.class);
 	@Autowired
 	private RedisTemplate<String, String> redisTemplate;
+	@Autowired
+	private RedisConnectionFactory factory;
 	@Test
 	public void test() {
-		redisTemplate.opsForValue().set("newtest", "1");
-		System.out.println(redisTemplate.opsForValue().get("newtest"));
+		redisTemplate.opsForValue().set("test_test", "测试", 60, TimeUnit.SECONDS);
+		String result=redisTemplate.opsForValue().get("test_test");
+		logger.debug("获取的缓存值是:{}",result);
+		setNx("test_test2", "测试2", Expiration.from(1000, TimeUnit.MILLISECONDS));
+		String result2=redisTemplate.opsForValue().get("test_test2");
+		logger.debug("获取的缓存值2是:{}",result2);
+	}
+	private boolean setNx( String key,  String value, final Expiration expiration) {
+		RedisSerializer keySerializer=redisTemplate.getKeySerializer();
+		RedisSerializer valueSerializer=redisTemplate.getValueSerializer();
+		logger.debug("要放到redis里的key[{}],value[{}],过期时间[单位:{},数值:{}]",key,value,expiration.getTimeUnit(),expiration.getExpirationTime());
+		final byte[] keyByte = keySerializer.serialize(key);
+		final byte[] valueByte = valueSerializer.serialize(value);
+		return redisTemplate.execute(new RedisCallback<Boolean>() {
+			@Override
+			public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
+				return connection.set(keyByte, valueByte, expiration, SetOption.SET_IF_ABSENT);
+			}
+		}, true);
+
 	}
 	
 }
